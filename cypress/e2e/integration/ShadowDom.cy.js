2bf9571757ca485154fa8ae80a57f561928905f0
MishraDurgesh1 <tarun.sahu@go.com.mt>
2023-06-23 11:12:09 +0530

    changes

--- cypress/e2e/integration/ShadowDom.cy.js
+++ cypress/e2e/integration/ShadowDom.cy.js
@@ -1,10 +1,10 @@
 describe("Implementation of Shadow Elements",()=>{
 beforeEach(()=>{
 cy.visit('https://selectorshub.com/iframe-in-shadow-dom/');
-})
+});
 
 it('Shadow Practice',()=>{
 cy.get('#userName').shadow().find('#kils').type('Durgesh');
 cy.get('#userName').shadow().find('#app2').shadow().find('#pizza').type('Mishraji');
-})
-})
+});
+});
